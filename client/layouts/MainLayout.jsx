64d6d8a5420441368fa7bc5546138a7a827863ff
Phil <warnerp18@gmail.com>
2016-04-17 11:38:16 -0500

    testing

--- client/layouts/MainLayout.jsx
+++ client/layouts/MainLayout.jsx
@@ -1,7 +1,7 @@
 import react, { Component } from 'react';
 
 export const MainLayout = ({content}) => (
-  <div className='container'>
+  <div className='container test'>
     {content}
   </div>
 )
