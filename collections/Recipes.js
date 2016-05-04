8bbac036e76dd32d94e50c1f45f28d90976ff999
Phil <warnerp18@gmail.com>
2016-05-03 19:44:28 -0500

    update collections; redirect on login/out

--- collections/Recipes.js
+++ collections/Recipes.js
@@ -3,6 +3,9 @@ Recipes = new Mongo.Collection('recipes');
 Recipes.allow({
   insert: function(userId, doc){
     return !!userId;
+  },
+  update: function(userId, doc){
+    return !!userId;
   }
 });
 
@@ -57,4 +60,13 @@ RecipeSchema = new SimpleSchema({
 	}
 });
 
+Meteor.methods({
+  toggleMenuItem: function(id, currentState){
+    Recipes.update(id, {
+      $set: {
+        inMenu: !currentState
+      }
+    });
+  }
+});
 Recipes.attachSchema( RecipeSchema );
